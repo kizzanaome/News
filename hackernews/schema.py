3c80c7a37e3ce96219db12603a17dc26ba97b125
Kizza Naume <naomekizzan@gmail.com>
2019-02-27 08:02:25 +0300

    Add user creation

---
+++ hackernews/schema.py
@@ -0,0 +1,24 @@
+import graphene
+
+import links.schema
+import users.schema
+import graphene
+import graphql_jwt
+
+
+class Query(users.schema.Query, links.schema.Query, graphene.ObjectType):
+    pass
+
+
+class Mutation(
+        links.schema.Mutation,
+        users.schema.Mutation,
+        graphene.ObjectType,
+):
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
---
+++ hackernews/urls.py
@@ -0,0 +1,11 @@
+"""hackernews URL Configuration
+"""
+from django.contrib import admin
+from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+from graphene_django.views import GraphQLView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('naome/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+]
